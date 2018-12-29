9aa9fa52cacbdf06899c652adf31d2dc43d4d212
BulletforFreedom <lyusikai@gmail.com>
2018-12-29 12:19:47 +0800

    something about faster-rcnn

---
+++ backbone/darknet.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 27 17:02:12 2018
+
+@author: lvsikai
+
+Email: lyusikai@gmail.com
+"""
+
+
+class darknet(object):
+    def __init__(self):
+        self.nothing=0
